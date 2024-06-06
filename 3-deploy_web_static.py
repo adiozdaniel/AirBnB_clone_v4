96330760b35dc6df3734cb79657d7825f74a4802
adiozdaniel <42722945+adiozdaniel@users.noreply.github.com>
2024-06-06 03:26:21 +0100

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['54.175.200.255', '100.25.21.215']
+env.hosts = ['34.232.52.236', '54.174.45.21']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
